4afddf9efdcb24695589fa2b0f710cd2ebafebed
nguyễn hiếu <nguyn hiu>
2016-05-09 16:03:36 +0700

    Fourth Commit

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -11,6 +11,7 @@ use App\Post;
 use App\Tag;
 use App\PostTag;
 use Auth;
+use Validator;
 
 class AdminController extends Controller
 {
@@ -22,60 +23,86 @@ class AdminController extends Controller
     	return view('admin.index',['posts'=>$posts]);
     }
     public function deleteXoa($id) {
-    	Post::find($id)->delete();
+        PostTag::where('post_id',$id)->delete();
+    	$post = Post::find($id);
+
+        unlink($post->image);
+
+        $post->delete();
+
     }
     public function getAddPost() {
         return view('admin.add-post');
     }
     public function postAddPost(Request $request) {
         
-        // dd($request->file('image'));
+        $rules = [
+            'title'       => 'required',
+            'image'       => 'required|image',
+            'description' => 'required',
+            'content'     => 'required'
+        ];
 
-        // dd('abc');
-        $post = new Post;
-        $post->title       = $request->input('title');
-        $post->description = $request->input('description');
-        $post->content     = $request->input('content');
-        $post->status      = $request->input('status');
-        $post->slug        = Str::slug($request->input('title') . '-' . time());
-        $post->user_id     = Auth::user()->id;
-        // if (count($request->input('tags')) > 0) {
-        //     foreach ($request->input('tags') as $key => $tag) {
-        //         $flag = Tag::where('name',$tag)->first();
-        //         if (!$flag) {
-        //             $tag = Tag::create([
-        //                 'name' => $tag
-        //             ]);
+        $messages = [
+            'title.required'       => 'Tiêu đề không được để trống',
+            'image.required'       => "Hình ảnh không được để trống",
+            'image.image'          => 'Chỉ được phép nhập hình ảnh',
+            'description.required' => "Tóm tắt không được để trống",
+            'content.required'     => 'Nội dung không được để trống'
+        ];
+        $validator = Validator::make($request->all(), $rules, $messages);
+        if($validator->fails()) {
+            return redirect()->back()->withInput()->withErrors($validator);
+        } else {
+            // dd('abc');
+            $post = new Post;
+            $post->title       = $request->input('title');
+            $post->description = $request->input('description');
+            $post->content     = $request->input('content');
+            $post->status      = $request->input('status');
+            $post->slug        = Str::slug($request->input('title') . '-' . time());
+            $post->user_id     = Auth::user()->id;
+            // if (count($request->input('tags')) > 0) {
+            //     foreach ($request->input('tags') as $key => $tag) {
+            //         $flag = Tag::where('name',$tag)->first();
+            //         if (!$flag) {
+            //             $tag = Tag::create([
+            //                 'name' => $tag
+            //             ]);
 
-        //         }
-        //     }
-        // }
-        // upload hình ảnh
-        $image = $request->file('image');
-        $filename = time() . $image->getClientOriginalExtension() . '.' . $image->getClientOriginalExtension();
-        $path = 'upload/images/';
-        $image-> move($path,$filename);
-        $post->image = $path . $filename;
-        $post->save();
+            //         }
+            //     }
+            // }
+            // upload hình ảnh
+            if ($request->hasFile('image')) {
+                $image = $request->file('image');
+                $filename = time() . $image->getClientOriginalExtension() . '.' . $image->getClientOriginalExtension();
+                $path = 'upload/images/';
+                $image-> move($path,$filename);
+                $post->image = $path . $filename;
+            }
+            
+            $post->save();
 
-        if (count($request->input('tags')) > 0) {
-            foreach ($request->input('tags') as $key => $tag) {
-                $flag = Tag::where('name',$tag)->first();
-                if (!$flag) {
-                    $tag = Tag::create([
-                        'name' => $tag
-                    ]);
-                    PostTag::create([
-                        'post_id' => $post->id,
-                        'tag_id' => $tag->id
-                    ]);
+            if (count($request->input('tags')) > 0) {
+                foreach ($request->input('tags') as $key => $tag) {
+                    $flag = Tag::where('name',$tag)->first();
+                    if (!$flag) {
+                        $tag = Tag::create([
+                            'name' => $tag
+                        ]);
+                        PostTag::create([
+                            'post_id' => $post->id,
+                            'tag_id' => $tag->id
+                        ]);
+                    }
                 }
             }
+            // PostTag::create([
+            //     'post_id' => $post->id,
+            //     'tag_id' => $tag->id
+            // ]);
+            return redirect()->to('admin');
         }
-        // PostTag::create([
-        //     'post_id' => $post->id,
-        //     'tag_id' => $tag->id
-        // ]);
-        dd('abc');
-    }
+    }  
 }
--- resources/views/admin/add-post.blade.php
+++ resources/views/admin/add-post.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.admin-index')
 @section('style')
 	<link rel="stylesheet" type="text/css" href="{{url('bootstrap-tagsinput.css')}}">
+	<style>
+		label.error {
+			color: red;
+			display: block;
+		}
+	</style>
 @endsection
 @section('content')
 
@@ -29,24 +35,36 @@
     </div>
     <div class="box-body">
   	
-		<form action="{{url('admin/add-post')}}" method="POST" role="form" enctype="multipart/form-data">
+		<form id="add-post" action="{{url('admin/add-post')}}" method="POST" role="form" enctype="multipart/form-data">
 			<legend>ADD</legend>
 			
 			{{csrf_field()}}
 			<div class="form-group">
 				<label for="">Tiêu đề</label>
+				@if ($errors->has('title'))
+					<p style="color: red;">{{$errors->first('title')}}</p>
+				@endif
 				<input type="text" class="form-control" id="title" placeholder="Tiêu đề" name="title">
 			</div>
 			<div class="form-group">
 				<label for="">Ảnh</label>
+				@if ($errors->has('image'))
+					<p style="color: red;">{{$errors->first('image')}}</p>
+				@endif
 				<input type="file" name="image" id="image">
 			</div>
 			<div class="form-group">
 				<label for="">Tóm tắt</label>
+				@if ($errors->has('description'))
+					<p style="color: red;">{{$errors->first('description')}}</p>
+				@endif
 				<textarea name="description" class="form-control" id="description" cols="30" rows="5"></textarea>
 			</div>
 			<div class="form-group">
 				<label for="">Nội dung</label>
+				@if ($errors->has('content'))
+					<p style="color: red;">{{$errors->first('content')}}</p>
+				@endif
 				<textarea name="content" class="form-control" id="content" cols="30" rows="5"></textarea>
 			</div>
 			<div class="form-group">
@@ -79,14 +97,68 @@
     <!-- CK Editor -->
     <script src="https://cdn.ckeditor.com/4.4.3/standard/ckeditor.js"></script>
     <script src="{{url('bootstrap-tagsinput.js')}}"></script>
-	
+	<!-- script jquery validate -->
+    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/jquery.validate.min.js"></script>
+    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/additional-methods.min.js"></script>
      <script>
       $(function () {
         // Replace the <textarea id="editor1"> with a CKEditor
         // instance, using default configuration.
         CKEDITOR.replace('content');
-        //bootstrap WYSIHTML5 - text editor
-        $(".textarea").wysihtml5();
+
       });
+    </script>
+	<!-- validate bằng jquery -->
+    <script>
+    	$('#add-post').validate({
+    		rules: {
+    			title: {
+    				required:true,
+    			},
+    			image: {
+    				required:true,
+    				accept: "image/*"
+    			},
+    			description: {
+    				required:true,
+    			},
+    			//jquery validate cho ckeditor
+    			content: {
+	                required: function() 
+	                {
+		                CKEDITOR.instances.content.updateElement();
+	                }
+                }
+            },
+    		messages: {
+    			title: {
+    				required: "Tiêu đề không được trống",
+    			},
+    			image: {
+    				required: "Ảnh không được trống",
+    				accept: "Chỉ được phép chọn ảnh"
+    			},
+    			description: {
+    				required: "Tóm tắt không được trống",
+    			},
+    			content: {
+    				required: "Nội dung không được trống",
+    			},
+    			 /* use below section if required to place the error*/
+                
+    		},
+    		errorPlacement: function(error, element) 
+                {
+                    if (element.attr("name") == "content") 
+                   {
+                    error.insertBefore("textarea#content");
+                    } else {
+                    error.insertBefore(element);
+                    }
+                },
+    		submitHandle: function(form) {
+    			$(form).submit();
+    		}
+    	});
     </script>
 @endsection
